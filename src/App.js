ab1d3287df7c90988a4797949b8b633238fdbb9e
umair arshad <ua220743@gmail.com>
2023-02-24 21:08:35 +0500

    update files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-import {Top_menu} from "./components/Top_menu";
+
 import {Mainbody} from './components/Mainbody';
-const obj={
-  title:"Softfixed",
-  source:"bootstarp"
-}
+
 function App() {
   return (
     
     <>
     
-  <Top_menu obj={obj}/>
+ 
   <Mainbody body="hello" />
     </>
   );
--- src/components/Mainbody.js
+++ src/components/Mainbody.js
@@ -1,39 +1,48 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 const Mainbody = (props) => {
-    const [value,setValue]=useState("Write text here");
-    function changeValue(){
-        let el=document.getElementById("textAreaExample6");
+    const [value, setValue] = useState("Write text here");
+    function changeValue() {
+        let el = document.getElementById("textAreaExample6");
         //console.log(el.value.toUpperCase());
         setValue(el.value);
     }
-    function uppercase(){
-        let el=document.getElementById("textAreaExample6");
+    function uppercase() {
+        let el = document.getElementById("textAreaExample6");
         console.log(el.value.toUpperCase());
         setValue(el.value.toUpperCase());
 
     }
-    function lowercase(){
-        let el=document.getElementById('textAreaExample6');
+    function lowercase() {
+        let el = document.getElementById('textAreaExample6');
         setValue(el.value.toLowerCase());
+    }
+    function reverseText() {
+        let reverse_text = "";
+        for (let i = 0; i < value.length; i++) {
+            reverse_text = reverse_text + value[value.length - (i + 1)];
+
+        }
+        console.log(reverse_text);
+        setValue(reverse_text);
     }
     return (
 
         <>
 
-            
 
 
-            <div className="form-outline mb-4 my- 6 w-75 my-5 justify-content-center "  style={{marginLeft:"12.5%"}}>
+
+            <div className="form-outline mb-4 my- 6 w-75 my-5 justify-content-center " style={{ marginLeft: "12.5%" }}>
                 <textarea className="form-control" id="textAreaExample6" rows="3" onChange={changeValue} value={value}></textarea>
-               
-                <input type="button" className="btn btn-primary my-4 d-inline " onClick={uppercase} value="To upper case"/> 
-                <input type="button" className="btn btn-primary my-4 d-inline mx-2" onClick={lowercase} value="To lower case"/> 
-                
-                
-                <p>{value.split(" ").length} word and {value.length} character</p>   
+
+                <input type="button" className="btn btn-primary my-4 d-inline " onClick={uppercase} value="To upper case" />
+                <input type="button" className="btn btn-primary my-4 d-inline mx-2" onClick={lowercase} value="To lower case" />
+                <input type="button" className="btn btn-primary my-4 d-inline mx-2" onClick={reverseText} value="Reverse text" />
+
+                <p>{value.split(" ").length} word and {value.length} character</p>
             </div>
-            
+
 
         </>
 
@@ -41,4 +50,4 @@ const Mainbody = (props) => {
 
 }
 
-export { Mainbody};
+export { Mainbody };
