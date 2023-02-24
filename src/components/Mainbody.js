b4ccd0819a842d802edaa41dddef9f99bcadde62
umair arshad <ua220743@gmail.com>
2023-02-24 20:48:14 +0500

    update files

--- src/components/Mainbody.js
+++ src/components/Mainbody.js
@@ -13,6 +13,10 @@ const Mainbody = (props) => {
         setValue(el.value.toUpperCase());
 
     }
+    function lowercase(){
+        let el=document.getElementById('textAreaExample6');
+        setValue(el.value.toLowerCase());
+    }
     return (
 
         <>
@@ -22,7 +26,11 @@ const Mainbody = (props) => {
 
             <div className="form-outline mb-4 my- 6 w-75 my-5 justify-content-center "  style={{marginLeft:"12.5%"}}>
                 <textarea className="form-control" id="textAreaExample6" rows="3" onChange={changeValue} value={value}></textarea>
-                <input type="button" className="btn btn-primary my-4" onClick={uppercase} value="Convert to upper case"/> 
+               
+                <input type="button" className="btn btn-primary my-4 d-inline " onClick={uppercase} value="To upper case"/> 
+                <input type="button" className="btn btn-primary my-4 d-inline mx-2" onClick={lowercase} value="To lower case"/> 
+                
+                
                 <p>{value.split(" ").length} word and {value.length} character</p>   
             </div>
             
